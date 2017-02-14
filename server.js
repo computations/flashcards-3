dabeae57bb2d3e0545704ae45ab8bb73c72c132f
Ben Bettisworth <bwbettisworth@alaska.edu>
2017-02-13 17:46:09 -0900

    basic hello world server put up

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.get('/index', function(req, res){
+    console.log(req);
+    res.send('Hello World!');
+})
+
+app.listen(3000, function(){
+    console.log("Example app listen on port 3000!");
+})
